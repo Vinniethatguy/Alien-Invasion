a13a282e778284ee9f6ec7dc2d4afd959de6d330
Vince <78511315+Vinniethatguy@users.noreply.github.com>
2022-01-12 23:44:14 -0600

    Delete settings.py

--- source code/settings.py
+++
@@ -1,8 +0,0 @@
-class Settings:
-    """""A class to store all settings for allen invasion"""
-    
-    def __init__(self):
-        """""Initialize the game's settings."""
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (230,230,230)
